6fad5b99b2054265ea7e1efbc33501ad6e8b1544
Spencer Tung <spencer.zh.tung@gmail.com>
2014-03-17 20:23:33 -0700

    About to start scraping JSON data from ConceptNet

--- punRecog.py
+++ punRecog.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #Recognizing puns
 
-import sys, random, nltk, urllib, string
+import sys, random, nltk, urllib, string, json
 from nltk.corpus import wordnet as wn
 
 # Narrow focus down to noun and verb homonyms?
@@ -11,25 +11,37 @@ from nltk.corpus import wordnet as wn
 
 # Start with sentences that are definitely puns
 
+
+# Checks a given word for potentially relevant concepts
+# Adds this word, with its associated concepts, to an ongoing dictionary
+#def conceptCheck:
+
+# Checks each word in a sentence against its expected POS vs how it appears on 
+# its own
+# If a word's POS differs against its expected POS, add it to a dictionary
+def POSCheck(sentence):
+	wordpos = 0
+	origSent = "Original sentence: " + sentence
+	print(origSent)
+	text = nltk.word_tokenize(sentence)
+	textTag = nltk.pos_tag(text)
+	print(textTag)
+	for word in text:
+		word = nltk.word_tokenize(word)
+		wordTag = nltk.pos_tag(word)
+		if (wordTag[0][1][:2] != textTag[wordpos][1][:2]):
+			diff = "Difference spotted at: " + wordTag[0][0]
+			print(diff)
+			print(wordTag[0][1])
+			print(textTag[wordpos][1])
+		wordpos += 1
+	print("\n")
+
 def main():
 	punIn = raw_input("Pun File: ") # get it it's a pun on "punning" hah hah
 	f = open(punIn, 'r')
-	wordpos = 0
 	for line in f:
-		origSent = "Original sentence: " + line
-		print(origSent)
-		text = nltk.word_tokenize(line)
-		textTag = nltk.pos_tag(text)
-		print(textTag)
-		for word in text:
-			print(word)
-			word = nltk.word_tokenize(word)
-			wordTag = nltk.pos_tag(word)
-			if (textTag[wordpos][1] != wordTag[0][1]):
-				diff = "Difference spotted at " + wordTag[0][0]
-				print(diff)
-			wordpos += 1
-		wordpos = 0
+		POSCheck(line)
 
 
 # Running the main function
